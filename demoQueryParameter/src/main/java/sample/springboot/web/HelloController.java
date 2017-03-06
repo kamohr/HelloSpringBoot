48fe865225ea2271c100dd27f5deb29a98432826
かもっち <kannyu@KANNYU-8>
2017-03-06 15:37:13 +0900

    initial import

---
+++ demoQueryParameter/src/main/java/sample/springboot/web/HelloController.java
@@ -0,0 +1,25 @@
+package sample.springboot.web;
+
+import java.util.Map;
+
+import org.springframework.util.MultiValueMap;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/hello")
+public class HelloController {
+
+    @RequestMapping(method=RequestMethod.GET)
+    public void getMethod(
+            @RequestParam String id,
+            @RequestParam Map<String, String> queryParameters,
+            @RequestParam MultiValueMap<String, String> multiMap) {
+
+        System.out.println("id=" + id);
+        System.out.println(queryParameters);
+        System.out.println(multiMap);
+    }
+}
