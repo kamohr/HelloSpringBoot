6796c0c26e026991ae85487a8ecf1968879fc482
かんゆ <kannyu@KANNYU-8>
2017-03-06 09:35:44 +0900

    demoMVCをポート番号変更用サンプルに変更

--- demoMVC/src/main/java/sample/springboot/web/HelloController.java
+++ demoMVC/src/main/java/sample/springboot/web/HelloController.java
@@ -1,25 +1,12 @@
 package sample.springboot.web;
 
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/hello")
 public class HelloController {
-
-    @RequestMapping(method=RequestMethod.GET)
-    public String getMethod() {
-        return "get";
-    }
-
-    @RequestMapping(method=RequestMethod.POST)
-    public String postMethod1() {
-        return "post";
-    }
-
-    @RequestMapping(value="/hey", method=RequestMethod.POST)
-    public String postMethod2() {
-        return "hey post";
+    @RequestMapping("/")
+    public String index() {
+        return "Hello Spring MVC!";
     }
 }
